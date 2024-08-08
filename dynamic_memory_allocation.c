947557de91da9fa37a504897723e1186e0909fb7
arpitparekh <arpitparekh9@gmail.com>
2024-08-08 11:33:59 +0530

    update

---
+++ dynamic_memory_allocation.c
@@ -0,0 +1,10 @@
+// C Dynamic Memory Allocation can be defined as a procedure
+// in which the size of a data structure (like Array) is changed during the runtime.
+
+#include <stdio.h>
+
+void main(){
+
+    
+
+}
